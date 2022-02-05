8915f1a43d7d791890834580888cd50024f373ed
Frederic Tu <69024184+gosqib@users.noreply.github.com>
2022-02-04 23:14:46 -0500

    Update discordbot.py

--- src/discordbot.py
+++ src/discordbot.py
@@ -5,75 +5,79 @@ import os
 
 from detector import Detector
 from webscrape import get_tenor
-from consts import EPILEPSY_DETECTION_REACTIONS
+from consts import EPILEPSY_DETECTION_REACTIONS, GIF_DANG_TRIG_REQ, GIF_DRAM_PIX_CHANGE, MP4_DANG_TRIG_REQ, MP4_DRAM_PIX_CHANGE
 
 bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready() -> None:
-    await bot.change_presence(status=discord.Status.invisible)
-    print(f'{bot.user} connected')
+	await bot.change_presence(status=discord.Status.invisible)
+	print(f'{bot.user} connected')
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-        
-    """
-    checks the first attachment of every message, if a video exists in its place,
-    perform a detection analysis, otherwise just pass
-    
-    if nothing was attached, there may still possibly be a tenor that was sent; that will be checked
-    """
-    if message.author == bot.user:
-        return
-    
-    attachments = message.attachments
-    file_mdta = ''
-    if not attachments:
-        message_text: str = message.content
+	if message.author == bot.user:
+		return
+		
+	"""
+	checks the first attachment of every message, if a video exists in its place,
+	perform a detection analysis, otherwise just pass
+	
+	if nothing was attached, there may still possibly be a tenor that was sent; that will be checked
+	"""
+	if message.author == bot.user:
+		return
+	
+	attachments = message.attachments
+	file_mdta = ''
+	if not attachments:
+		message_text: str = message.content
 
-        if message_text.startswith('https://tenor.com/'): #if true, message text would be the wanted link
-            # check for epilepsy in this gif through webscraping
+		if message_text.startswith('https://tenor.com/'): #if true, message text would be the wanted link
+			# check for epilepsy in this gif through webscraping
+			
+			file_mdta = get_tenor(link=message_text)
+			gif = cv.VideoCapture(file_mdta)
 
-            file_mdta = get_tenor(link=message_text)
-            gif = cv.VideoCapture(file_mdta)
-            check = Detector(gif).epilepsy()
-            # print(check)
-            if check:
-                for reaction in EPILEPSY_DETECTION_REACTIONS:
-                    await message.add_reaction(reaction)
-            
-            gif=None # killing the opencv control over this gif file to be able to delete the file
-            os.remove(file_mdta)
+			# adjustments for gifs since they're different from .mp4
+			check = Detector(gif).epilepsy(is_gif=True)
+			# print(check)
 
-        return # once the analysis of a theoretical gif is done, 
-               # or if the message doesn't have any attachments or tenor links, pass this message
+			if check:
+				for reaction in EPILEPSY_DETECTION_REACTIONS:
+					await message.add_reaction(reaction)
+			
+			gif=None # killing the opencv control over this gif file to be able to delete the file
+			os.remove(file_mdta)
 
-    video = message.attachments[0]
-    if video.content_type != 'video/mp4':
-        return
+		return # once the analysis of a theoretical gif is done, 
+			   # or if the message doesn't have any attachments or tenor links, pass this message
 
-    file_name = video.filename
-    if os.path.isfile(file_name):
-        file_name = f'T{file_name}'
-        await video.save(file_name)
-    else: 
-        await video.save(file_name)
+	video = message.attachments[0]
+	if video.content_type != 'video/mp4':
+		return
 
-    check = Detector(cv.VideoCapture(file_name)).epilepsy()
-    if check:
-        for reaction in EPILEPSY_DETECTION_REACTIONS:
-            await message.add_reaction(reaction)
+	file_name = video.filename
+	if os.path.isfile(file_name):
+		file_name = f'T{file_name}'
+		await video.save(file_name)
+	else: 
+		await video.save(file_name)
 
-    os.remove(file_name)
+	# readjust for .mp4 since the gifs changed adjustments
+	check = Detector(cv.VideoCapture(file_name)).epilepsy()
+	if check:
+		for reaction in EPILEPSY_DETECTION_REACTIONS:
+			await message.add_reaction(reaction)
+
+	os.remove(file_name)
 
 
 
 
 @bot.command()
 async def bruh(ctx):
-    await ctx.send(bot.user)
+	await ctx.send(bot.user)
 
-TOKEN = "[ADD_YOUR_TOKEN_HERE]"
+TOKEN = "OTM2NDQzMDE0NzY1NzU2NDE2.YfNQhA.Seeg93GeJMVPbI_jQA8E3gw2_54"
 bot.run(TOKEN)
