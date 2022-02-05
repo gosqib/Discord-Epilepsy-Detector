cecb3164e83d4e3ab37f3bd7af70de7b94e1c97c
Frederic Tu <69024184+gosqib@users.noreply.github.com>
2022-02-05 16:45:30 -0500

    Update webscrape.py

--- src/webscrape.py
+++ src/webscrape.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup#type:ignore
 from urllib.request import urlopen
 import requests#type:ignore
 
-def get_tenor(link: str) -> str:
+from keyvars.mytypes import FileName
+
+def get_tenor(link: str) -> FileName:
     """tenor is the name of discord text gifs
     this function webscrapes that gif since it's not possible to extract is from the 
     discord message component. after the source has been extracted from the html, this
@@ -12,16 +14,17 @@ def get_tenor(link: str) -> str:
     html = urlopen(link).read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
     gif = soup.find_all('div', class_='Gif')[0]
+
+    gif = soup.find_all('div', class_='Gif')[0]
     download_location = gif.next['src']
+
     file_name = (
         link
             .replace('/', '')
             .replace(':', '')
     )
-    
-    # save gif
     with open(file_name, 'wb') as f:
         f.write(requests.get(download_location).content)
     
-    return file_name # this will be passed into a method that will require the file name
-                     # to work on the file
+    return file_name # this will be passed into a method that will require the file name in 
+                     # the current directory
