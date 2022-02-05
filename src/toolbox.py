d34a4c552c9b228d74d8058fead8f02e4dfd2d19
Frederic Tu <69024184+gosqib@users.noreply.github.com>
2022-02-05 16:45:11 -0500

    Update toolbox.py

--- src/toolbox.py
+++ src/toolbox.py
@@ -2,8 +2,6 @@ import cv2 as cv #type:ignore
 from typing import Generator
 import numpy as np
 
-from consts import *
-
 def get_frames(video: cv.VideoCapture) -> Generator[np.ndarray, None, None]:
 	while 1:
 		status, frame = video.read()
@@ -11,4 +9,3 @@ def get_frames(video: cv.VideoCapture) -> Generator[np.ndarray, None, None]:
 			break
 		
 		yield frame
-
